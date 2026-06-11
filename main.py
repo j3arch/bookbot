e01dd0c7fcbf71665dfa9f668de76d7ee7c3314e
j3arch <j3archx@gmail.com>
2026-06-12 00:31:31 +0300

    small fix

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import sys
-from stats import count_words, character_count, character_sorted 
+from stats import (
+    count_words,
+    character_count,
+    character_sorted
+)
 
 
 def main() -> None:
